e9cd96db10531d729dfafdc03ddc95bb1a403c78
Nathan <nathandvj1@gmail.com>
2025-05-29 08:26:49 -0400

    theme color overrated

--- app/reverb/page.tsx
+++ app/reverb/page.tsx
@@ -81,7 +81,6 @@ export default function Home() {
 			<meta property="og:image" content="https://quiteboring.dev/reverb-banner.png" />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:image" content="https://quiteboring.dev/reverb-banner.png" />
-			<meta name="theme-color" content="#151C62" />
    <link rel="icon" href="/reverb.ico" />
 			
 			<Nav />
